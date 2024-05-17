23014783ee6b65ec6850d361acd98cd4d9c13e2b
andy <andersgee@gmail.com>
2024-05-17 15:38:44 +0200

    couple of pages

---
+++ src/app/guestbook/page.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  searchParams: Record<string, string | string[] | undefined>;
+  /** adjust params according dynamic routes, eg if this is under a [slug] folder */
+  params: { slug: string };
+};
+
+export default function Page({ params }: Props) {
+  return <div>guestbook</div>;
+}
