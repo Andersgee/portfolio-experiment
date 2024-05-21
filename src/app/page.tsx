39880cfa1cc53113e22f5135a8621dbfb73a861b
andy <andersgee@gmail.com>
2024-05-21 15:37:00 +0200

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Page() {
   return (
     <>
-      <header className="">
+      <section className="">
         <div className="">
           <h1 className="mb-6 text-4xl tracking-tighter text-color-neutral-800">Kristin Eklöf</h1>
           <p className="text-color-neutral-1000 ">Artificial Intelligence and Real World Knowledge</p>
         </div>
-      </header>
+      </section>
       <main className="mt-40 xl:mt-72">
         <section className="">
           <h2 className="my-6 text-2xl tracking-tighter">Stuff</h2>
